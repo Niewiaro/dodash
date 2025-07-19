d8b6f2a3791ffb912b8d43bbebcca9d9c69b09fb
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-07-19 15:42:03 +0200

    [feat] add AuthorizeNavigation component and integrate with layout

---
+++ app/components/AuthorizeNavigation.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items = ref<NavigationMenuItem[]>([
+	{
+		label: "Guide",
+		icon: "i-lucide-house",
+		to: "/",
+	},
+	{
+		label: "GitHub",
+		icon: "i-simple-icons-github",
+		badge: "Niewiaro",
+		to: "https://github.com/Niewiaro",
+		target: "_blank",
+	},
+]);
+</script>
+
+<template>
+	<UNavigationMenu
+		highlight
+		:items="items"
+		class="w-full justify-center"
+	/>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AuthorizeNavigation from "~/components/AuthorizeNavigation.vue";
+</script>
+
+<template>
+	<div>
+		<AuthorizeNavigation />
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+
+</style>
