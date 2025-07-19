f2675bbfcb55a17bf496dadb8e570efd23fb7a6d
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-07-19 14:21:53 +0200

    [chore] add ESLint configuration for stylistic rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
 })
